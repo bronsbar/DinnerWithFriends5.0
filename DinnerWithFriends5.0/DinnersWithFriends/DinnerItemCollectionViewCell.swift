449014d21f4c3b242858e3c1fca1875d5d57ddac
Bart Bronselaer <Bart@macbook-pro-van-bart.local>
2018-01-09 20:30:31 +0100

    collectionviews

---
+++ DinnerWithFriends5.0/DinnersWithFriends/DinnerItemCollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  DinnerItemCollectionViewCell.swift
+//  DinnerWithFriends5.0
+//
+//  Created by Bart Bronselaer on 9/01/18.
+//  Copyright © 2018 Bart Bronselaer. All rights reserved.
+//
+
+import UIKit
+
+class DinnerItemCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var image: UIImageView!
+    @IBOutlet weak var label: UILabel!
+}
